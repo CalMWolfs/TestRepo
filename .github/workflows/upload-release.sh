98549180126d1f6b9f10b91498e938b1a251d21d
calwolfson <cwolfson58@gmail.com>
2024-12-27 10:32:18 -0500

    logging release param

--- .github/workflows/upload-release.sh
+++ .github/workflows/upload-release.sh
@@ -4,6 +4,8 @@ preReleaseParam=""
 if ! echo "${UPDATE_VERSION}" | grep -E '.*\.0'>/dev/null; then
   preReleaseParam="--prerelease"
   echo "This is a pre-release."
+else
+  echo "This is a release."
 fi
 
 # todo MULTI-VERSION
@@ -13,7 +15,7 @@ TARGET_NAME="build/libs/TestRepo-${UPDATE_VERSION}.jar"
 extra_notes=$(cat build/update-notes.txt)
 
 if [ -f "${TARGET_NAME}" ]; then
-  gh release create -t "SkyHanni ${UPDATE_VERSION}" --verify-tag "${UPDATE_VERSION}" --prerelease --draft --notes "$extra_notes" "${TARGET_NAME}"
+  gh release create -t "SkyHanni ${UPDATE_VERSION}" --verify-tag "${UPDATE_VERSION}" ${preReleaseParam} --draft --notes "$extra_notes" "${TARGET_NAME}"
 else
   echo "Error: File ${TARGET_NAME} does not exist."
   exit 1
