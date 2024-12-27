dbeb1244f22d2222bd10198d441fed797fff4185
calwolfson <cwolfson58@gmail.com>
2024-12-27 10:26:56 -0500

    hard code prerelease

--- .github/workflows/upload-release.sh
+++ .github/workflows/upload-release.sh
@@ -3,6 +3,7 @@
 preReleaseParam=""
 if ! echo "${UPDATE_VERSION}" | grep -E '.*\.0'>/dev/null; then
   preReleaseParam="--prerelease"
+  echo "This is a pre-release."
 fi
 
 # todo MULTI-VERSION
@@ -12,8 +13,7 @@ TARGET_NAME="build/libs/TestRepo-${UPDATE_VERSION}.jar"
 extra_notes=$(cat build/update-notes.txt)
 
 if [ -f "${TARGET_NAME}" ]; then
-  gh release create -t "SkyHanni ${UPDATE_VERSION}" --verify-tag "${UPDATE_VERSION}" --draft \
-    --notes "$extra_notes" $preReleaseParam "${TARGET_NAME}"
+  gh release create -t "SkyHanni ${UPDATE_VERSION}" --verify-tag "${UPDATE_VERSION}" --prerelease --draft --notes "$extra_notes" "${TARGET_NAME}"
 else
   echo "Error: File ${TARGET_NAME} does not exist."
   exit 1
